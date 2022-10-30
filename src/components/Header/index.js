f3957d6c0f5aac0e083950f395344212bf56792c
CorentinBAHIER <corentin.bahier81@gmail.com>
2022-10-30 17:18:47 +0100

    mise en place du style du composant header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,14 @@
 // == Import
 import './header.scss';
-import logoDesktop from '../../assets/images/logodéfinitif.png';
+import logoDesktop from '../../assets/images/LogoManette.png';
 
 // == Composant
 function Header() {
   return (
     <div className="header">
-      <button type="button"> Menu </button>
-      <img src={logoDesktop} alt="Logo du site" />
-      <button type="button"> Login </button>
+      <button className="header-button" type="button"> Menu </button>
+      <img className="header-img" src={logoDesktop} alt="Logo du site" />
+      <button className="header-button" type="button"> Login </button>
     </div>
   );
 }
