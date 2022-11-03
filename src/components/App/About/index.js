71ddcc6fb6c9792fcb151adb6fb7cdba0c6e3dd7
CeliaHmc <celiabouzar@gmail.com>
2022-11-03 09:37:47 +0100

    Résolution d'un bug sur le bandeau Contactez Nous

---
+++ src/components/App/About/index.js
@@ -0,0 +1,12 @@
+// == Import
+import './about.scss';
+
+// == Composant
+function About() {
+  return (
+    <h1 className="home-title">A propos</h1>
+  );
+}
+
+// == Export
+export default About;
