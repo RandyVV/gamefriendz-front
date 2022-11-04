e06c74957c05801fedd233b14761e6979f36beb6
CeliaHmc <celiabouzar@gmail.com>
2022-11-04 15:42:51 +0100

    Intégration de la page cgu & politique de confidentialité

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,6 +12,7 @@ import Contact from './Contact';
 import About from './About';
 import Login from './Login';
 import Signup from './Signup';
+import Cgu from './Cgu';
 
 // == Composant
 function App() {
@@ -33,6 +34,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/cgu" element={<Cgu />} />
       </Routes>
       <Footer />
     </div>
