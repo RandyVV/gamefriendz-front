dd26e377393942613d88b802f98ec1e99ec34b6e
CeliaHmc <celiabouzar@gmail.com>
2022-11-17 10:13:08 +0100

    Ajout d'alertes authentification reussie/identifiants incorrects

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -21,9 +21,11 @@ const user = (store) => (next) => (action) => {
         .then((response) => {
           store.dispatch(connectUser(response.data.token, response.data.data));
           store.dispatch(foundUserDatas());
+          alert('Connexion réussie !');
         })
         .catch((error) => {
           console.log(error);
+          alert('Identifiants incorrects');
         });
 
       next(action);
@@ -50,6 +52,7 @@ const user = (store) => (next) => (action) => {
       })
         .then((response) => {
           console.log(response.data.pseudo);
+          alert('Votre compte a bien été créé');
         })
         .catch((error) => {
           console.log(error);
